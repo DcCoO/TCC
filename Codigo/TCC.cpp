e5def89ad2062322ff092ed46a17292aea06358a
DcCoO <dcc4@cin.ufpe.br>
2019-04-29 03:16:56 -0300

    Update TCC.cpp

--- Codigo/TCC.cpp
+++ Codigo/TCC.cpp
@@ -1,201 +1,175 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define debug 1
 #define foreach(exp) for(auto exp)
-#define pa(a) if(debug){cout<<#a ":\t"; foreach(x : a) cout<<x<<" "; cout<<endl;}
-#define pb(a) if(debug){ cout<<#a ": ";             \
-    for(int i = 0; i < 2 * N - 1; i++) {            \
-        if(i % lg(2 * N - 1) == 0 && i) cout<<"| "; \
-        cout<<a[i]<<" ";                            \
-    } cout<<endl;                                   \
+#define MAXSIZE 1010
+#define pv(v, s) for(int ye = 0; ye < s; ye++){ cout<<v[ye]<<" "; } cout<<endl<<endl;
+#define pm(m) foreach(v : m){ foreach(e : v){ cout<<e<<" "; } cout<<endl; } cout<<endl;
+#define pc(c) foreach(m : c){ foreach(v : m){ foreach(e : v){ cout<<e<<" "; } cout<<endl; } cout<<"--------------"<<endl; } cout<<endl;
+
+
+inline int lg(int n){return (int) floor(log2(n));}
+
+//arvore inicial
+vector<int> tree[MAXSIZE];
+int parent[MAXSIZE];
+
+int n;
+
+//vertices visitados pela dfs
+int dfsPath[2 * MAXSIZE - 1];
+//nivel dos vertices visitados pela dfs
+int dfsLevel[MAXSIZE];
+//indice da primeira ocorrencia de cada vertice dfs
+int dfsIndex[MAXSIZE];
+
+int position;
+
+void dfs(int u = 0, int level = 0){
+    dfsPath[position] = u;
+    dfsLevel[u] = level;
+    dfsIndex[u] = position++;
+
+    foreach(v : tree[u]){
+        dfs(v, level + 1);
+        dfsPath[position++] = u;
+    }
 }
 
-int lg(int n){
-    return (int) floor(log2(n));
-}
-
-
-int N, bucketSize, bucketCount;
-vector<vector<int> > tree;
+//tabela para preprocessar cada bloco internamente
+vector<vector<vector<int> > > blocktable;
+//tabela para preprocessar cada bloco
 vector<vector<int> > table;
-vector<int> level, node, index, minForward, minBackward;
 
-///index: guarda posicao da primeira ocorrencia
-///de um no na dfs
-
-int lowest(int i, int j, int k = -1){
-    int db = level[i] <= level[j] ? node[i] : node[j];
-    return k == -1? db : level[db] <= level[k] ? node[db] : node[k];
+//dados dois vertices, retorna o de menor nivel
+int get_lowest(int u, int v, int w = -1){
+    if(w != -1) return get_lowest(dfsLevel[u] <= dfsLevel[w] ? u : w, v);
+    return dfsLevel[u] <= dfsLevel[v] ? u : v;
 }
 
-///inicializacao da arvore
-void init(){
-    tree.clear(); table.clear(); level.clear(); node.clear();
-    index.clear(); minForward.clear(); minBackward.clear();
-    cin>>N;
-    tree = vector<vector<int> >(N);
-    index = vector<int> (N, INT_MAX);
-    int c, f;
-    for(int i = 0; i < N; i++){
-        cin>>c;
-        //cout<<i<<" tem "<<c<<" filhos\n";
-        while(c--){
-            cin>>f;
-            tree[i].push_back(f);
-        }
-    }
-    pa(tree[0]);
-}
+void build_blocktable(){
+    int SIZE = 2 * n - 1;
+    int blockSize = lg(SIZE);
+    int numBlocks = SIZE / blockSize + (SIZE % blockSize != 0);
+    int blockWidth = lg(blockSize) + 1;
+    //numBlocks blocos de tamanho blockSize x blockWidth
+    blocktable = vector<vector<vector<int> > >(numBlocks);
+    for(int i = 0; i < numBlocks; i++) blocktable[i] = vector<vector<int> >(blockSize);
+    for(int i = 0; i < numBlocks; i++) for(int j = 0; j < blockSize; j++) blocktable[i][j] = vector<int>(blockWidth);
 
-inline int h(int u){return level[index[u]];}
-
-///apos essa dfs, teremos duas listas:
-///node: lista composta de todo caminho da dfs
-///level: lista do nivel de cada no em node
-///ambos tem tamanho 2n-1
-void dfs(int u, int l = 0){
-    node.push_back(u); level.push_back(l);
-    index[u] = min(index[u], (int) node.size() - 1);
-    foreach(v : tree[u]) {
-        dfs(v, l + 1);
-        node.push_back(u); level.push_back(l);
-    }
-}
-
-
-
-void build_minimum(){
-    bucketSize = lg(2 * N - 1);
-    bucketCount = ((2 * N - 1) / bucketSize) + ((2 * N - 1) % bucketSize != 0);
-    table = vector< vector<int> >(bucketCount);  table[0] = vector<int>();
-
-    minForward = vector<int>(2 * N - 1, INT_MAX);
-    minBackward = vector<int>(2 * N - 1, INT_MAX);
-
-    ///gerar os minimos para tras
-    for(int bucket = 0; bucket < bucketCount; bucket++){
-        //cout<<"Bucket "<<bucket<<endl;
-        int i = bucket * bucketSize;
-        minBackward[i] = node[i++];
-        for(int len = min(i + bucketSize - 1, 2 * N - 1); i < len; i++){
-            //cout<<"mB[i] = "<<level[i]<<" <= "<<level[ minBackward[i - 1] ]<<" ? "<<node[i]<<" : "<<minBackward[i - 1]<<endl;
-
-            minBackward[i] = h(node[i]) < h(minBackward[i - 1]) ? node[i] : minBackward[i - 1];
-        }
-        ///preenchendo primeira linha da sparse table
-        table[0].push_back(minBackward[i - 1]);
-    }
-
-    ///gerar os minimos para frente
-    for(int bucket = 0; bucket < bucketCount; bucket++){
-        //cout<<"Bucket "<<bucket<<endl;
-        int i = min((bucket + 1) * bucketSize, 2 * N - 1) - 1;
-        //cout<<"i = "<<i<<endl;
-        minForward[i] = node[i--];
-        for(int len = bucket * bucketSize; i >= len; i--){
-            //cout<<"mF["<<i<<"] = "<<level[index[minForward[i + 1]]] <<" < " << level[i]<<" ?"<<endl;
-            //cout<<"\t"<<minForward[i + 1]<<" : "<<node[i]<<endl;
-
-            //cout<<"mF["<<i<<"] = "<<level[i]<<" <= "<<level[ minForward[i + 1] ]<<" ? "<<node[i]<<" : "<<minBackward[i - 1]<<endl;
-            minForward[i] = h(node[i]) < h(minForward[i + 1]) ? node[i] : minForward[i + 1];
-
+    position = 0;
+    for(int block = 0; block < numBlocks; block++){
+        for(int i = 0; i < blockSize; i++) blocktable[block][i][0] = dfsPath[min(position++, 2 * n - 2)];
+        int power = 1;
+        for(int i = 1; i < blockWidth; i++){    //para cada coluna
+            for(int j = 0, len = blockSize - (power << 1); j <= len; j++){
+                blocktable[block][j][i] = get_lowest(blocktable[block][j][i - 1], blocktable[block][j + power][i - 1]);
+            }
+            power <<= 1;
         }
     }
 }
 
-///constroi uma sparse table de tamanho (n/lg(n)) x lg(n/lg(n))
-void build_sparse_table(){
-
-    for(int i = 1, len = lg(table[0].size()) + 1; i < len; i++){
-        table[i] = vector<int>();               ///2^i
-        for(int j = 0, len = table[0].size() - (1 << i) + 1; j < len; j++){
-            //cout<<"limit = "<<len<<endl;
-            //cout<<"table["<<i<<"]["<<j<<"] = min("<<table[i - 1][j]<<", "<<table[i - 1][j + pow(2, i - 1)]<<")\n";
-            table[i].push_back(min(table[i - 1][j], table[i - 1][j + (1 << (i - 1))]));
-        }
-    }
-    //cout<<endl;
-    /*
-    for(int i = 0; i < table.size(); i++){
-        for(int j = 0; j < table[i].size(); j++){
-            cout<<table[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    */
+int block_query(int block, int l, int r){
+    int num = r - l + 1;
+    int col = floor(log2(num));
+    return get_lowest(blocktable[block][l][col], blocktable[block][l + num - (1<<col)][col]);
 }
 
-///o LCA tem 2 casos:
-///1 - ambos os nos no mesmo bucket
-///2 - nos em buckets diferentes
+void build_table(){
+    int SIZE = 2 * n - 1;
+    int blockSize = lg(SIZE);
+    int numBlocks = SIZE / blockSize + (SIZE % blockSize != 0);
+    int cols = lg(numBlocks) + 1;
+
+    //tabela de tamanho numBlocks x (lg(numBlocks) + 1)
+    table = vector<vector<int> >(numBlocks);
+    for(int i = 0; i < numBlocks; i++) table[i] = vector<int>(cols);
+
+    for(int i = 0; i < numBlocks; i++) table[i][0] = block_query(i, 0, blockSize - 1);
+
+    int power = 1;
+    for(int i = 1; i < cols; i++){    //para cada coluna
+        for(int j = 0, len = numBlocks - (power << 1); j <= len; j++){
+            table[j][i] = get_lowest(table[j][i - 1], table[j + power][i - 1]);
+        }
+        power <<= 1;
+    }
+}
+
+int multiblock_query(int lblock, int rblock){
+
+    int num = rblock - lblock + 1;
+    int col = floor(log2(num));
+    return get_lowest(table[lblock][col], table[lblock + num - (1<<col)][col]);
+}
+
 int LCA(int u, int v){
-    //cout<<"LCA("<<u<<","<<v<<")"<<endl;
-    ///extraimos o index real de u e v na dfs
-    int uindex = index[u], vindex = index[v];
-    if(uindex > vindex) swap(uindex, vindex);
+    int SIZE = 2 * n - 1;
+    int blockSize = lg(SIZE);
 
-    //cout<<"\tindex["<<u<<"] = "<<uindex<<", index["<<v<<"] = "<<vindex<<endl;
-    ///calculamos em qual bucket eles estao
-    int ubucket = uindex / bucketSize, vbucket = vindex / bucketSize;
-    //cout<<"\tbucket["<<u<<"] = "<<ubucket<<", bucket["<<v<<"] = "<<vbucket<<endl;
-    ///caso 1: O(lg(n))
-    if(ubucket == vbucket){
-        //cout<<"caso1"<<endl;
-        int lca = uindex;
-        for(int i = uindex + 1; i <= vindex; i++){
-            lca = level[ node[i] ] < level[ node[lca] ] ? i : lca;
-        }
-        return node[lca];
-    }
-    ///caso 2: O(1)
-    else{
+    int l = dfsIndex[u], r = dfsIndex[v]; if(l > r) swap(l, r);
+    int lblock = l / blockSize, rblock = r / blockSize;
 
-        /*cout<<"\tCaso 2: ";
-        for(int i = 0; i < 2 * N - 1; i++){
-            if(i == uindex) cout<<"| ";
-            cout<<node[i]<<" ";
-            if(i == vindex) cout<<"| ";
-        }cout<<endl;
-        */
+    //caso 1: consulta num unico bloco
+    if(lblock == rblock) return block_query(lblock, l % blockSize, r % blockSize);
 
-        /// [  u---][][][][][---v  ]
-        int leftMin = minForward[uindex], rightMin = minBackward[vindex];
-        //cout<<"\tleftMin = "<<leftMin<<", rightMin = "<<rightMin<<endl;
-        int left = ubucket + 1, right = vbucket - 1;
-        int edgeMin = h(leftMin) < h(rightMin) ? leftMin : rightMin;
-        ///se nao existir buckets entre eles
-        if(left > right) return edgeMin;
-        ///caso contrario
-        else {
-            int diff = right - left + 1;
-            //cout<<"\tdiferenca: "<<diff<<endl;
-            int col = lg(diff);
-            int minInside =
-            h(table[col][left]) < h(table[col][left + diff - (1<<col)]) ?
-              table[col][left]  :   table[col][left + diff - (1<<col)];
-            //cout<<"\tminInside = "<<minInside<<endl;
-            //cout<<endl<<endl;
-            return level[index[minInside]] < level[index[edgeMin]] ? minInside : edgeMin;
-        }
-    }
+    int lmin = block_query(lblock, l % blockSize, blockSize - 1);
+    int rmin = block_query(rblock, 0, r % blockSize);
+    //caso 2: consulta em blocos adjacentes
+    if(lblock + 1 == rblock) return get_lowest(lmin, rmin);
+    //caso 3: consulta em blocos distantes
+    return get_lowest(lmin, multiblock_query(lblock + 1, rblock - 1), rmin);
 }
 
+void init(){
+    position = 0;
+    int s, q, u, v; cin>>n;
+    for(int i = 0; i < n; i++) tree[i].clear();
+    /*
+    tree[0].push_back(1);tree[0].push_back(2);tree[0].push_back(3);
+    tree[2].push_back(4);
+    tree[3].push_back(5);tree[3].push_back(6);
+    tree[6].push_back(7);tree[6].push_back(8);
+    tree[8].push_back(9);
+    */
 
+    for(int i = 0; i < n; i++) parent[i] = i;
+    for(int i = 0; i < n; i++) {
+        cin>>s;
+        while(s--){
+            cin>>q; q--;
+            parent[q] = i;
+            tree[i].push_back(q);
+        }
+    }
 
-int main(){
+    int root = 0;
+    while(root != parent[root]) root = parent[root];
 
-    init();
-    //cout<<"DFS\n";
-    dfs(0);
-    build_minimum();
-    build_sparse_table();
+    if(n > 1){
+        dfs(root);
+        build_blocktable();
+        build_table();
+    }
+    //pv(dfsPath, 2 * n - 1);
+    //pv(index, n);
+    //pm(table);
+    //pc(blocktable);
 
-    int q, l, r;
     cin>>q;
     while(q--){
-        cin>>l>>r;
-        cout<<LCA(l,r)<<endl;
+        cin>>u>>v;
+
+        if(n > 1) cout<<(LCA(u - 1, v - 1) + 1)<<endl;
+        else cout<<1<<endl;
     }
+}
 
-
+int main(){
+    int t;
+    cin>>t;
+    for(int caso = 1; caso <= t; caso++){
+        cout<<"Case "<<caso<<":"<<endl;
+        init();
+    }
 }
